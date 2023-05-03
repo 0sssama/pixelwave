00a26013e3091ba545d1c7a1f5f07bb5d3cae3ba
osama <0ssama@protonmail.com>
2023-05-03 14:51:03 +0100

    chore: adding logo and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
-    <main className="Home flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="main-text text-6xl font-bold">
-        Welcome to <strong>Pixelwave</strong>!
+    <main className="Home flex flex-col items-center justify-center min-h-screen py-2 !gap-0">
+      <img
+        alt="Pixelwave Icon"
+        src="/assets/icon.svg"
+        className="max-w-[10rem]"
+      />
+      <h1 className="main-text mt-10">
+        Welcome to <strong className="cap">Pixelwave</strong>!
       </h1>
     </main>
   );
