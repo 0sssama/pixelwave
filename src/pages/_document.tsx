90dc80bf1a5ff95b218b0bb74644271f7041a4be
osama <0ssama@protonmail.com>
2023-05-03 15:21:53 +0100

    chore: adding SEO meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,21 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Pixelwave - Agency of your dreams</title>
+        <title>Pixelwave - The no-code agency you needed</title>
+        <meta
+          name="description"
+          content="Pixelwave is an agency specialized in crafting beautiful user interfaces and seamless user experiences for complex web applications using no-code technologies."
+        />
+        <meta
+          name="og:title"
+          content="Pixelwave - The no-code agency you needed"
+        />
+        <meta
+          name="og:description"
+          content="Pixelwave is an agency specialized in crafting beautiful user interfaces and seamless user experiences for complex web applications using no-code technologies."
+        />
+        <meta name="og:url" content="pixelwave.io" />
+        <meta name="og:image" content="/assets/brand.png" />
       </Head>
       <body>
         <Main />
