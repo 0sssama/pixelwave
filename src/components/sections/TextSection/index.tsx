e0833b71beae7c60fe1f594e45d628c70ebb0b74
osama <0ssama@protonmail.com>
2023-05-08 22:33:31 +0100

    chore: finished text section animations

--- src/components/sections/TextSection/index.tsx
+++ src/components/sections/TextSection/index.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import { globalTransition } from "@/utils/globalTransition";
+import { globalVariant } from "@/utils/globalVariant";
+import { useInViewControl } from "@/utils/useInViewControl";
+import { motion } from "framer-motion";
 
 function TextSection() {
+  const { ref, control } = useInViewControl({ threshold: 0.4 });
+
   return (
-    <div className="Text Section">
-      <h1>
+    <div className="Text Section" ref={ref}>
+      <motion.h1
+        variants={globalVariant}
+        initial="hidden"
+        animate={control}
+        transition={{
+          ...globalTransition,
+          delay: 0.4,
+        }}
+      >
         And if you need a <b>hand</b>, we can provide you with{" "}
         <strong>ongoing help</strong> and optimization.
-      </h1>
-      <p>We call it Bubble development on demand.</p>
+      </motion.h1>
+      <motion.p
+        variants={globalVariant}
+        initial="hidden"
+        animate={control}
+        transition={{
+          ...globalTransition,
+          delay: 0.6,
+        }}
+      >
+        We call it Bubble development on demand.
+      </motion.p>
     </div>
   );
 }
