1c64bdf163debdb412a16a8c29ca4d256df2332c
osama <0ssama@protonmail.com>
2023-05-08 22:31:06 +0100

    chore: finished text section, responsive

--- src/components/sections/TextSection/index.tsx
+++ src/components/sections/TextSection/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function TextSection() {
-  return <div className="Text Section">TextSection</div>;
+  return (
+    <div className="Text Section">
+      <h1>
+        And if you need a <b>hand</b>, we can provide you with{" "}
+        <strong>ongoing help</strong> and optimization.
+      </h1>
+      <p>We call it Bubble development on demand.</p>
+    </div>
+  );
 }
 
 export default TextSection;
