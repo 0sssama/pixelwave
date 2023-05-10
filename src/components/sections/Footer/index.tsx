ad8591bbd74cf293509c4742ac81317fbdfc52fe
osama <0ssama@protonmail.com>
2023-05-10 22:00:06 +0100

    chore: finished footer + animation + responsive

--- src/components/sections/Footer/index.tsx
+++ src/components/sections/Footer/index.tsx
@@ -1,7 +1,69 @@
-import React from "react";
+import { Button } from "@/components/atoms";
+import { globalTransition } from "@/utils/globalTransition";
+import { globalVariant } from "@/utils/globalVariant";
+import { useInViewControl } from "@/utils/useInViewControl";
+import { motion } from "framer-motion";
 
 function Footer() {
-  return <div className="Footer Section"></div>;
+  const { ref, control } = useInViewControl();
+
+  return (
+    <div className="Footer Section" ref={ref}>
+      <motion.h1
+        variants={globalVariant}
+        initial="hidden"
+        animate={control}
+        transition={{
+          ...globalTransition,
+          delay: 0.6,
+        }}
+      >
+        HAVE A <strong>PROJECT</strong> IN MIND?
+      </motion.h1>
+      <motion.div
+        className="CTA"
+        variants={globalVariant}
+        initial="hidden"
+        animate={control}
+        transition={{
+          ...globalTransition,
+          delay: 1,
+        }}
+      >
+        <p>
+          We&apos;d love to help you bring it to life!
+          <br />
+          Book a free 15 minutes discovery call.
+        </p>
+        <Button variant="primary" href="#">
+          Let&apos;s talk!
+        </Button>
+      </motion.div>
+      <motion.div
+        className="bottom w-full flex flex-col items-center"
+        variants={globalVariant}
+        initial="hidden"
+        animate={control}
+        transition={{
+          ...globalTransition,
+          delay: 1.4,
+        }}
+      >
+        {/* <div className="socialmedia flex gap-2 items-center">
+
+		</div> */}
+        <p>
+          &copy; {new Date().getFullYear()} Pixelwave | All rights reserved.
+        </p>
+      </motion.div>
+    </div>
+  );
 }
+
+// const socialMedia = [
+// 	{
+// 		name: "Facebook",
+// 	}
+// ]
 
 export default Footer;
